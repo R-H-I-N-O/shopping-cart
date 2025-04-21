6b8a23fe7612bfe274c5f91ee3eea14537532dec
R-H-I-N-O <sirunni@gmail.com>
2025-04-21 15:45:40 +0530

    minor layout adjustment for responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import productList from "./_config/productData";
 export default function Home() {
   return <div className="w-full h-full">
     <Navbar/>
-    <main className="h-full p-16 grid-cols-1 md:grid-cols-2 grid lg:grid-cols-3 gap-16 place-items-center box-border">
+    <main className="h-full p-16 grid-cols-1 md:grid-cols-2 grid xl:grid-cols-3 gap-16 place-items-center box-border">
       {productList.map((product, index)=>(
         <ProductCard id={product.id} name={product.name} imgUrl={product.imgUrl} description={product.description} price={product.price} key={`product-${index}`}/>
       )) }
